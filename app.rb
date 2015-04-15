76d412feca91ccac9e597f9e14d8adbbe449d6f7
Sam Backus <sbackus@kayak.com>
2015-04-15 10:42:26 -0400

    add home page

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 
+get '/' do 
+	"this is a survey app built with sinatra. </br>"+
+	"see: https://github.com/sbackus/survey </br>"+
+	"<a href='/form'>Take the survey </a>"
+end
+
 get '/form' do
   erb :form
 end
