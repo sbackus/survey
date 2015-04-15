8f3d634a29c43692aae5822aa4e59b0e80d2e417
Sam Backus <sbackus@kayak.com>
2015-04-15 10:30:05 -0400

    save data to data.txt

--- app.rb
+++ app.rb
@@ -4,3 +4,14 @@ require 'sinatra'
 get '/form' do
   erb :form
 end
+
+post '/form' do
+   file = File.open('data.txt', 'a')
+   file.puts params[:message]
+  "You said '#{params[:message]}'"
+end
+
+get '/data' do 
+	file = File.open('data.txt', 'r')
+	file.read
+end
